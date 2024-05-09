faa0cb3f765c65254c0e232ec1cd52f23aabea6d
Caio Vidal Silva <caiovidal.dev@gmail.com>
2024-05-09 00:20:28 -0300

    Atualizando repositorio

--- src/main/java/com/example/JavaBD/repository/TarefaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.JavaBD.repository;
-
-import com.example.JavaBD.model.Tarefa;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TarefaRepository extends JpaRepository<Tarefa, Long> {
-}
